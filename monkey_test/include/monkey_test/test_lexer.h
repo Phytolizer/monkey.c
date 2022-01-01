48b634d1d20c6e21ea0edee8e1878e9321094ac2
Kyle Coffey <mariorules24@gmail.com>
2022-01-01 14:10:18 -0600

    segment tests

---
+++ monkey_test/include/monkey_test/test_lexer.h
@@ -0,0 +1,8 @@
+#ifndef MONKEY_TEST_LEXER_H_
+#define MONKEY_TEST_LEXER_H_
+
+#include <test/test.h>
+
+test_func(lexer_next_token);
+
+#endif  // MONKEY_TEST_LEXER_H_
---
+++ monkey_test/source/test_lexer.c
@@ -0,0 +1,45 @@
+#include "monkey_test/test_lexer.h"
+
+#include <inttypes.h>
+#include <monkey/lexer.h>
+#include <monkey/token.h>
+#include <next_token_test.h>
+#include <test/test.h>
+
+test_func(lexer_next_token) {
+  struct {
+    MkTokenType expected_type;
+    const char* expected_literal;
+  } tests[] = {
+      {mk_token_assign, "="}, {mk_token_plus, "+"},      {mk_token_lparen, "("},
+      {mk_token_rparen, ")"}, {mk_token_lbrace, "{"},    {mk_token_rbrace, "}"},
+      {mk_token_comma, ","},  {mk_token_semicolon, ";"}, {mk_token_eof, ""},
+  };
+
+  MkLexer l = {0};
+  MkLexerInit(&l, (StringView){
+                      .begin = next_token_test,
+                      .end = next_token_test + next_token_test_size,
+                  });
+  for (uint64_t i = 0; i < sizeof(tests) / sizeof(tests[0]); i++) {
+    MkToken t = MkLexerNextToken(&l);
+    test_assert(
+        StringEqual(t.type, tests[i].expected_type),
+        do {
+          MkTokenFree(t);
+          MkTokenTypesManage(kTokenTypesFree);
+        } while (false),
+        "tests[%" PRIu64 "].expected_type: %" STRING_FMT
+        ", t.type: %" STRING_FMT,
+        i, STRING_PRINT(tests[i].expected_type), STRING_PRINT(t.type));
+    test_assert(
+        StringEqualView(t.literal, StringViewFromC(tests[i].expected_literal)),
+        do {
+          MkTokenFree(t);
+          MkTokenTypesManage(kTokenTypesFree);
+        } while (false),
+        "tests[%" PRIu64 "].expected_literal: %s, t.literal: %" STRING_FMT, i,
+        tests[i].expected_literal, STRING_PRINT(t.literal));
+  }
+  test_pass();
+}
