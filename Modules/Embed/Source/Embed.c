b10e64ffafb706e02c59902c310238da81742858
Kyle Coffey <me@phytolizer.dev>
2022-01-30 11:38:32 -0600

    Finish up the embed script

--- Modules/Embed/Source/Embed.c
+++ Modules/Embed/Source/Embed.c
@@ -31,6 +31,28 @@ String StringFromSpan(StringSpan span)
     return s;
 }
 
+String StringFromC(const char* c)
+{
+    size_t n = strlen(c);
+    String s = {
+        .data = calloc(1, n + 1),
+        .capacity = n,
+        .length = n,
+    };
+    memcpy(s.data, c, n);
+    return s;
+}
+
+String StringInitN(size_t n)
+{
+    String s = {
+        .data = calloc(1, n + 1),
+        .capacity = n + 1,
+        .length = 0,
+    };
+    return s;
+}
+
 void StringAlloc(String* s)
 {
     size_t oldCapacity = s->capacity;
@@ -45,6 +67,22 @@ void StringAlloc(String* s)
 
 void StringAppendC(String* s, const char* c)
 {
+    size_t n = strlen(c);
+    while (s->length + n + 1 > s->capacity)
+    {
+        StringAlloc(s);
+    }
+
+    memcpy(s->data + s->length, c, n);
+    s->length += n;
+}
+
+void StringFree(String* s)
+{
+    free(s->data);
+    s->data = NULL;
+    s->capacity = 0;
+    s->length = 0;
 }
 
 typedef struct
@@ -98,7 +136,7 @@ int main(int argc, char** argv)
     Arguments args = ArgumentsInit(argc, argv);
 
     StringSpan programName = NextArgument(&args);
-    if (args.count < 3)
+    if (args.count < 2)
     {
         fprintf(stderr, "Usage: " SS_FMT " <file> <outputBaseName>\n", SS_ARG(programName));
         ArgumentsComplete(&args);
@@ -108,10 +146,86 @@ int main(int argc, char** argv)
     StringSpan fileName = NextArgument(&args);
     StringSpan outputBaseName = NextArgument(&args);
 
+    String fileNameStr = StringFromSpan(fileName);
     FILE* input = fopen(fileName.data, "r");
-    String headerOutput = StringFromSpan(outputBaseName);
-    StringAppendC(&headerOutput, ".h");
+    if (input == NULL)
+    {
+        fprintf(stderr, "Failed to open %s", fileNameStr.data);
+        StringFree(&fileNameStr);
+        return 1;
+    }
+    StringFree(&fileNameStr);
 
+    fseek(input, 0, SEEK_END);
+    size_t inputLen = ftell(input);
+    fseek(input, 0, SEEK_SET);
+    String inputText = StringInitN(inputLen);
+    fread(inputText.data, 1, inputLen, input);
+    inputText.length = inputLen;
+    fclose(input);
+
+    String headerOutputName = StringFromSpan(outputBaseName);
+    StringAppendC(&headerOutputName, ".h");
+    FILE* headerOutput = fopen(headerOutputName.data, "w");
+
+    char buffer[1024] = {0};
+    size_t cursor = 0;
+    const char* loser = strrchr(outputBaseName.data, '\\');
+    if (loser == NULL)
+    {
+        loser = strrchr(outputBaseName.data, '/');
+    }
+    if (loser == NULL)
+    {
+        loser = outputBaseName.data;
+    }
+    else
+    {
+        loser++;
+    }
+    fprintf(headerOutput, "#pragma once\nextern const char %s[];\n", loser);
+    fclose(headerOutput);
+
+    String sourceFile = StringFromSpan(outputBaseName);
+    StringAppendC(&sourceFile, ".c");
+    FILE* sourceOutput = fopen(sourceFile.data, "w");
+    fprintf(sourceOutput, "#include \"%s\"\nconst char %s[] = \"", headerOutputName.data, loser);
+    StringFree(&headerOutputName);
+    for (size_t i = 0; i < inputText.length; ++i)
+    {
+        if (cursor > sizeof(buffer) - 2)
+        {
+            fwrite(buffer, 1, cursor, sourceOutput);
+            cursor = 0;
+        }
+        switch (inputText.data[i])
+        {
+        case '\n':
+            buffer[cursor++] = '\\';
+            buffer[cursor++] = 'n';
+            break;
+        case '\r':
+            buffer[cursor++] = '\\';
+            buffer[cursor++] = 'r';
+            break;
+        case '\t':
+            buffer[cursor++] = '\\';
+            buffer[cursor++] = 't';
+            break;
+        case '"':
+            buffer[cursor++] = '\\';
+            buffer[cursor++] = '"';
+            break;
+        default:
+            buffer[cursor++] = inputText.data[i];
+            break;
+        }
+    }
+    fwrite(buffer, 1, cursor, sourceOutput);
+    fprintf(sourceOutput, "\";\n");
+    fclose(sourceOutput);
+
+    StringFree(&inputText);
     ArgumentsComplete(&args);
     return 0;
 }
