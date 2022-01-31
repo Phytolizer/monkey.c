0a87c05420e1adcfad754c885d42a8d6c7c943e6
Kyle Coffey <me@phytolizer.dev>
2022-01-31 09:35:45 -0600

    Add some comments to the Main.c of MonkeyTest.

--- Modules/MonkeyTest/Source/Main.c
+++ Modules/MonkeyTest/Source/Main.c
@@ -4,14 +4,23 @@
 
 int main(void)
 {
+    // Dump leaks to stderr.
     LEAK_SET_STDERR();
+
+    // Initialize the test state.
+    // This tracks test passes and an assertion count.
     TestState testState = {0};
+
+    // Run the tests.
     TEST_RUN_SUITE(Lexer, &testState);
 
+    // Verify the absence of leaks.
     if (LEAK_CHECK())
     {
         return 1;
     }
+
     TEST_SUMMARY(&testState);
+    // Return 0 on success.
     return testState.numFailures;
 }
